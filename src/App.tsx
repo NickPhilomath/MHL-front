8e4a657817cb54d1dc011d3041250a6823793780
Dragon <akbarisroilovdragon@gmail.com>
2023-12-09 11:19:46 -0500

    www

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 import Table from "./components/Table";
 import Sidebar from "./components/Sidebar";
 
+const Url = "http://127.0.0.1:8000/api/trucks";
+
 function App() {
+  useEffect(() => {
+    const controller = new AbortController();
+    axios
+      .get(Url, { signal: controller.signal })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+
+    return () => controller.abort();
+  }, []);
+
   return (
     <>
       <Sidebar />
